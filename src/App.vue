efc5ce2a22b81e37ad8ada01735c9793f4bed1bd
fcojavierMT <fcojavierdev@gmail.com>
2018-09-15 23:33:43 +0200

    feat(home-page): added new tabs

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <NavBar></NavBar>
-    </div>
     <router-view/>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,52 @@
 <template>
-  <div class="home">
-    <HelloWorld/>
+  <div>
+    <b-container fluid 
+                 class="p-4 bg-dark animated slideInLeft">
+      <b-row>
+        <b-col class="animated jackInTheBox slower">
+          <b-img thumbnail rounded fluid 
+                width="120" 
+                height="120" 
+                :src="require('../assets/imgs/profile.jpg')" 
+                alt="Thumbnail" />
+        </b-col>
+      </b-row>
+    </b-container>
+    <div class="about animated fadeInUp">
+      <p>Hi!, my name is <b>Francisco Javier Martínez Talavera</b>, but people usually call me 'Talavera'</p>
+      <p>And im a Junior Programmer, focused on web technologies as JavaScript, HTML, CSS...etc</p>
+      <p>Im currently Working as a Frontend Developer, on Valencia (Spain)</p>
+      <h4><strong>Some of my works</strong></h4>
+    </div>
+    <b-container fluid class="animated fadeInRight slower">
+      <b-tabs>
+        <b-tab no-body title="Vue+Firebase" active>
+          <b-img class="card-image" 
+                 rounded fluid 
+                 width="150" 
+                 height="150" 
+                 :src="require('../assets/imgs/firebaseVue.png')" 
+                 alt="Thumbnail" />
+          <b-card-footer>
+            First Work of all my car
+          </b-card-footer>
+        </b-tab>
+      </b-tabs>
+    </b-container>
   </div>
 </template>
 
-<script>
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+<style lang="scss" scoped>
+  .about {
+    padding: 20px;
+
+    > p {
+      font-weight: normal;
+    }
   }
-}
-</script>
+
+  .card-image {
+    margin: 30px;
+  }
+</style>
