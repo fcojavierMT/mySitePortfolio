0290792cbdd623cb8ade3c945c38eab2592a9557
fcojavierMT <fcojavierdev@gmail.com>
2019-01-20 16:10:25 +0100

    feat(jumbo): added jumbotron

--- src/components/works.vue
+++ src/components/works.vue
@@ -1,22 +1,24 @@
 <template>
   <b-container class="p-4 animated zoomIn delay-1s">
-    <h4>{{$t('works')}}</h4>
-    <b-row>
-      <b-col v-for="work in works" :key="work.title">
-        <p>{{work.title}}</p>
-        <b-img
-          rounded
-          thumbnail
-          :src="require(`@/assets/imgs/works/${work.image}.jpg`)"
-          alt="Thumbnail"
-        />
-        <div class="info-container">
-          <a :href="work.link" target="_blank" rel="external">
-            <i class="fab fa-github fa-2x"></i>
-          </a>
-        </div>
-      </b-col>
-    </b-row>
+    <b-jumbotron>
+      <h5>{{$t('works')}}</h5>
+      <b-row>
+        <b-col v-for="work in works" :key="work.title">
+          <p>{{work.title}}</p>
+          <b-img
+            rounded
+            thumbnail
+            :src="require(`@/assets/imgs/works/${work.image}.jpg`)"
+            alt="Thumbnail"
+          />
+          <div class="info-container">
+            <a :href="work.link" target="_blank" rel="external">
+              <i class="fab fa-github fa-2x"></i>
+            </a>
+          </div>
+        </b-col>
+      </b-row>
+    </b-jumbotron>
   </b-container>
 </template>
 
@@ -30,7 +32,7 @@ export default {
           title: "Vue + Firebase",
           image: "stickNote",
           link: "https://github.com/fcojavierMT/proyecto-final"
-        }
+        },
       ]
     };
   }
